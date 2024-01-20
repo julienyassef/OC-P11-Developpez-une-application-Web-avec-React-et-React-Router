2e36140a89eed3c1270a20a0db56d5eec6ff20ce
julien yassef <juyassef@gmail.com>
2024-01-20 12:14:15 +0100

    ajout data.map dans Home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,11 +2,21 @@ import "./home.css"
 import Thumbnail from '../components/Thumbnail'
 import SloganBanner from '../components/SloganBanner'
 
+// Import des données JSON
+import data from '../data/data.json'
+
 function Home() {
   return (
     <div className="home">
       <SloganBanner/>
-      <Thumbnail/>
+      {data.map(item => (
+        <Thumbnail
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          cover={item.cover}
+        />
+      ))}
     </div>
   )
 }
