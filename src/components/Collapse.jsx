e8a5a3d8b290a7667dfb9cc27dae3d7a2a787d42
julien yassef <juyassef@gmail.com>
2024-01-22 11:16:53 +0100

    collapse modif

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,16 +1,23 @@
-import './Collapse.css'
+import './Collapse.css';
 import { useState } from 'react';
-import arrow from "../assets/arrow.svg"
+import arrow from "../assets/arrow.svg";
 
-const Collapse = ({ title, children }) => {
+const Collapse = ({ title, information }) => {
   const [isCollapsed, setCollapsed] = useState(true);
 
   return (
     <div className='collapse'>
-      <span className='title'>{title}toto</span>
-      <img src={arrow} alt="fléche" className='arrow' onClick={() => setCollapsed(!isCollapsed)} />
-      {!isCollapsed && <div>{children}</div>}
-    </div>
+      <div className='banner-collapse'>
+      <span className='title'>{title}</span>
+      <img
+        src={arrow}
+        alt="flèche"
+        className={`arrow ${isCollapsed ? '' : 'rotate'}`}
+        onClick={() => setCollapsed(!isCollapsed)}
+      />
+      </div>
+      {!isCollapsed && <div className='information'>{information}</div>}
+      </div>
   );
 };
 
--- src/page/About.jsx
+++ src/page/About.jsx
@@ -2,6 +2,8 @@ import './About.css'
 import pictureAbout from '../assets/pictureAbout.png'
 import Collapse from '../components/Collapse'
 
+// Import des données JSON
+import dataAbout from '../data/dataAbout.json'
 
 function About() {
   return (
@@ -10,7 +12,9 @@ function About() {
       <div className='darken-banner-about'></div>
       <img className="img-banner-about" src= {pictureAbout} alt="photo montagne"/>
       </div>
-      <Collapse/>
+      {dataAbout.map((item, index) => (
+        <Collapse key={index} title={item.title} information={item.information} />
+      ))}
     </div>
   )
 }
