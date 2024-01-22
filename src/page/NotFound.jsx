b77d0fb265af04092d0db6fcc18c9b3603969a83
julien yassef <juyassef@gmail.com>
2024-01-22 01:57:24 +0100

    création de la page 404

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -1,6 +1,13 @@
+import './notFound.css'
+import { NavLink } from 'react-router-dom'
+
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="notFound-page">
+      <p className="txt-404">404</p>
+      <p className="txt-notFound">Oups! La page que vous demandez n'existe pas.</p>
+      <NavLink className="link-notFound" to="/">Retourner sur la page d'accueil</NavLink>
+    </div>
   )
 }
 export default NotFound
