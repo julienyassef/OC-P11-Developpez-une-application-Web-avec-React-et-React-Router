ad1a1410856a775a130eb3c10281ff514f17c3eb
julien yassef <juyassef@gmail.com>
2024-01-23 19:08:16 +0100

    création component host et son css

--- src/components/Host.jsx
+++ src/components/Host.jsx
@@ -1,8 +1,19 @@
 import "./Host.css";
 
-function Host(name, picture) {
+function Host({ name, picture }) {
+  // Diviser le nom en deux parties (nom et prénom)
+  const [firstName, lastName] = name.split(" ");
+
   return (
-    <div>Host</div>
-  )
+    <div className="container-host">
+      <div className="container-host-name">
+        <p className="name-host">{firstName}</p>
+        <p className="name-host">{lastName}</p>
+      </div>
+      <img className="picture-host" src={picture} alt={`photo de ${name}`} />
+    </div>
+  );
 }
-export default Host
+
+export default Host;
+
