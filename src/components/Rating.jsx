6d0f489a7c11d425828ed5764e657d51f0850edd
julien yassef <juyassef@gmail.com>
2024-01-23 17:41:04 +0100

    création component rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,8 +1,21 @@
 import "./Rating.css";
 
-function Rating(count) {
+function Rating({ count }) {
+  // Créer un tableau de 5 étoiles
+  const stars = Array(5).fill(null);
+
   return (
-    <div>Rating</div>
-  )
+    <div className="rating-container">
+      {stars.map((e, index) => (
+        <img
+          key={index + 1}
+          src={index + 1 <= count ? "/src/assets/star-full.svg" : "/src/assets/star-empty.svg"}
+          alt={`Star ${index + 1}`}
+        />
+      ))}
+    </div>
+  );
 }
-export default Rating
+
+export default Rating;
+
