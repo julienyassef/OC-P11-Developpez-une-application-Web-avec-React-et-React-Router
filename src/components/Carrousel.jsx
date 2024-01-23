b77a3cc20125f2140bb4271893e72823fd691217
julien yassef <juyassef@gmail.com>
2024-01-23 12:28:23 +0100

    carrousel: si il ya juste une image, ne pas afficher les arrow

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -18,11 +18,17 @@ function Carrousel({ pictures }) {
   };
 
   return (
-    <div className="carrousel">
+  <div className="carrousel">
+  {pictures.length > 1 ? (
+    <>
       <img className="carrousel-picture" src={pictures[currentIndex]} alt={`Image ${currentIndex + 1}`} />
-      <img className="carrousel-arrow carrousel-arrow-left" src={arrow} alt="arrow" onClick={goToPrevious}/>
-      <img className="carrousel-arrow carrousel-arrow-right" src={arrow} alt="arrow" onClick={goToNext}/>
-    </div>
+      <img className="carrousel-arrow carrousel-arrow-left" src={arrow} alt="arrow" onClick={goToPrevious} />
+      <img className="carrousel-arrow carrousel-arrow-right" src={arrow} alt="arrow" onClick={goToNext} />
+    </>
+  ) : (
+    <img className="carrousel-picture" src={pictures[currentIndex]} alt={`Image ${currentIndex + 1}`} />
+  )}
+</div>
   );
 }
 
